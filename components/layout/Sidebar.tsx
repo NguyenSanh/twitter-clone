7f932c5d9c80c8111c4cd67d78a2fc9ccaf89b70
NguyenSanh <jsn006@ucsd.edu>
2023-08-16 00:11:11 -0700

    Sidebar & SidebarLogo Changes

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -2,6 +2,9 @@
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
 
+/* Import SidebarLogo component */
+import SidebarLogo from './SidebarLogo';
+
 const Sidebar = () => {
 
     // Create an array of objects:
@@ -24,7 +27,28 @@ const Sidebar = () => {
     ]
 
     return ( 
-        <div>
+        /*
+        col-span-{n} makes an element span n columns
+        h-full sets the element's height to be 100% of its parent's height
+        pr-4 (padding-right) class sets 4 padding to the right side of the element
+        md:pr-6 class applies additional 6 padding to the right when 'md' screen-with is reached
+        */
+        <div className="col-span-1 h-full pr-4 md:pr-6">
+            {/* 
+            'flex' class applies flexbox styling to the element 
+            'flex-col' class sets the flex direction to column
+            'items-end' aligns flex items to the end of the cross axis (horizontal in this case)
+            */}
+            <div className='flex flex-col items-end'>
+                {/* 
+                'space-y-2' sets margin-top to 0.5rem (4px)
+                'lq:w-[230px] sets width of element to 230px when screen width is at least lg breakpoint, lg targets larger screens
+                */}
+                <div className='space-y-2 lg:w-[230px]'>
+                    <SidebarLogo />
+                </div>
+
+            </div>
 
         </div>
     );
---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,33 @@
+/* Importing useRouter hook */
+import { useRouter } from "next/router";
+
+/* Importing BsTwitter icon from the react-icons library */
+import { BsTwitter } from "react-icons/bs";
+
+const SidebarLogo = () => {
+    /* useRouter hook is used to access a router object in any function */
+    const router = useRouter();
+
+    return ( 
+        /* 
+        'rounded-full' makes the element have fully rounded corners, creating a circular shape
+        'h-14' sets height to 14
+        'w-4' sets width to 14
+        'p-4' sets 4 units of padding to all sides of the element
+        'flex' makes an element a flex container with direction row
+        'justify-center' centers flex-items horizontally
+        'items-center' centers flex-items vertically
+        'hover:bg-blue-300' sets background color to blue when hovered over
+        'hover:bg-opacity-10' reduces opacity to 10 when hovered over
+        'cursor-pointer' sets cursor to a pointer when hovering over this div element
+        'transition' adds a smooth transition effect when ......
+         */
+        <div className="rounded-full h-14 w-14 p-4 flex items-center justify-center 
+        hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition">
+            {/* Insert Twitter icon from react-icons library */}
+            <BsTwitter size={30} color="white"/>
+        </div>
+     );
+}
+ 
+export default SidebarLogo;
